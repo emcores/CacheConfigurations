8f94c315ecbe60929e681d134cb3e4b0f3dde637
Islam <ibadreldin@ufl.edu>
2019-02-25 23:41:24 -0500

    Add execute command and base config

--- Dataset.jl
+++ Dataset.jl
@@ -1,5 +1,22 @@
 module Dataset
 
+struct CacheConfig
+    l1d_size::String
+    l1i_size::String
+    l1d_assoc::Int
+    l1i_assoc::Int
+    cacheline_size::Int
+end
+
+const base_cacheconfig = CacheConfig("8kB","8kB",4,4,32)
+
+struct Gem5Build
+    build_root::String
+    gem5opt::String
+    gem5fast::String
+    se_script::String
+end
+
 function clone_gem5(build_root)
     cwd = pwd()
     cd(build_root)
@@ -28,7 +45,27 @@ function build_gem5_x86(build_root)
      end
     # build gem5 using scons
     @time run(`scons build/X86/gem5.opt build/X86/gem5.fast -j$(Sys.CPU_THREADS)`)
+    gem5b = Gem5Build(
+        build_root,
+        joinpath(build_root,"gem5","build","X86","gem5opt"),
+        joinpath(build_root,"gem5","build","X86","gem5fast"),
+        joinpath(build_root,"gem5","configs","example","se.py")
+    )
     cd(cwd)
+    return gem5b
+end
+
+function exec_gem5opt_command(gem5b::Gem5Build,
+    cacheconfig::CacheConfig, outdir::String, exec::String, options::String)
+
+    c = cacheconfig
+    command = `$(gem5b.gem5opt) --outdir=$outdir
+        $(gem5b.se_script) -c $exec -o "$options"
+        --l1d_size $(c.l1d_size) --l1i_size $(c.l1i_size)
+        --l1d_assoc $(c.l1d_assoc) --l1i_assoc $(c.l1i_assoc)
+        --cacheline_size $(c.cacheline_size)
+        --caches`
+    run(command)
 end
 
 end
--- generate_dataset.jl
+++ generate_dataset.jl
@@ -4,4 +4,10 @@ import .Dataset
 
 root = mkpath("build")
 Dataset.clone_gem5(root)
-Dataset.build_gem5_x86(root)
+gem5b = Dataset.build_gem5_x86(root)
+Dataset.exec_gem5opt_command(
+    gem5b, Dataset.base_cacheconfig, joinpath(root,"gem5out"),
+    build/gem5/tests/test-progs/hello/bin/x86/linux/hello
+    joinpath(root,"gem5","build","gem5","tests","test-progs","hello","bin","x86","linux","hello"),
+    ""
+)
